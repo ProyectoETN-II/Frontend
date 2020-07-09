ff3582e4f36e1e9c2aeac7129e69de21cb9434b2
AngelJuanMa <angelcarballo66666@gmail.com>
2020-07-09 02:52:34 -0300

    Auto-generated commit

--- dist/out-tsc/src/app/models/follow.js
+++
@@ -1,8 +0,0 @@
-export class Follow {
-    constructor(_id, user, followed) {
-        this._id = _id;
-        this.user = user;
-        this.followed = followed;
-    }
-}
-//# sourceMappingURL=follow.js.map
--- dist/out-tsc/src/app/models/like.js
+++
@@ -1,9 +0,0 @@
-export class Like {
-    constructor(_id, user, liked, publication) {
-        this._id = _id;
-        this.user = user;
-        this.liked = liked;
-        this.publication = publication;
-    }
-}
-//# sourceMappingURL=like.js.map
